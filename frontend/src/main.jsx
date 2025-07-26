b6dcf7a1f21ebf553a7eca20b27ed2a64132570b
Shafeequemk80 <shafeequemk80@gmail.com>
2025-07-26 17:42:53 +0530

    font added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import "./global.css";
 import "./index.css";
+import "./fonts.css";
+
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 
