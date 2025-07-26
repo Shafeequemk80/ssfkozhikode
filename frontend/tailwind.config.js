35cc8bcf5034bd1c318c779f2fa66d254489c3a0
Shafeequemk80 <shafeequemk80@gmail.com>
2025-07-26 17:00:59 +0530

    changed vite congig

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     ],
   theme: {
-    extend: {},
+     extend: {
+      fontFamily: {
+        fractul: ['Fractul', 'sans-serif'],
+      },
+    },
   },
   plugins: [
   ],
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // or wherever your files are
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        fractul: ['Fractul', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
